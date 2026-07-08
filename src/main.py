994d7f708c5149f18b246ae8b0095073084ca712
ares-dev-temp <jisairc54@gmail.com>
2026-07-07 23:26:49 -0500

    attempt 1

--- src/main.py
+++ src/main.py
@@ -17,7 +17,9 @@ def main():
     #generate_page( "content/index.md", "template.html", "public/index.html" )
     #generate_page_recursive( "content", "template.html", "public" )
 
-    my_doc = "/https://github.com/ares-dev-temp/doc/"
+    #my_doc = "/https://github.com/ares-dev-temp/doc/"
+
+    base_path = "/https://github.com/" + base_path
 
     generate_page_recursive( "content", "template.html", "docs", base_path )
 
